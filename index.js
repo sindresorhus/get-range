838ad033663539e9ef7f2cabb4a31ba4a89b9b7d
Franklin Davenport <sparebytes@gmail.com>
2021-08-27 20:06:11 -0500

    pr fixes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const validate = (name, value) => {
 		throw new TypeError(`Expected \`${name}\` to be a safe integer`);
 	}
 };
+const lt = (l, r) => l < r;
+const gt = (l, r) => l > r;
 
 export default function * getRange({start = 0, end, step = 1}) {
 	if (step === 0) {
@@ -12,10 +14,10 @@ export default function * getRange({start = 0, end, step = 1}) {
 	validate('start', start);
 	validate('end', end);
 	validate('step', step);
-	
-	const compare = step < 0 ? (i) => i > end : (i) => i < end;
 
-	for (let index = start; compare(index); index += step) {
+	const compare = step < 0 ? gt : lt;
+
+	for (let index = start; compare(index, end); index += step) {
 		yield index;
 	}
 }
